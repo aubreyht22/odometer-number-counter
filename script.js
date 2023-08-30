3ab61172475fe6dd05a32aafe77a4a6c3d4a2795
Aubrey Thompson <35383782+aubreyht22@users.noreply.github.com>
2023-08-30 12:15:39 -0400

    Update script.js
    set ending number to current year

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 setTimeout(function(){
-    odometer.innerHTML = 2099;//ending number
-}, 1000);//last argument is for time in milliseconds or seconds depending on the need
+    let year = new Date().getFullYear();
+    odometer.innerHTML = year;
+}, 1000);
